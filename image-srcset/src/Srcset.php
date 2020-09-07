03d5c0c7bf5f0952b515f0966ada527fd2e56c1d
GUILLAUME NICOLAS <guillaume.96@live.fr>
2020-09-07 22:08:40 +0200

    Update Srcset.php

--- image-srcset/src/Srcset.php
+++ image-srcset/src/Srcset.php
@@ -10,7 +10,7 @@ use GN\Image;
  */
 class Srcset
 {
-	const RESIZE_UPDATE = true; // true -> resize images every call
+	const RESIZE_UPDATE = false; // true -> resize images every call
 	const DIR_SAVE_IMG = "/assets/img-@generated"; // from document_root
 	const JPG_COMPRESSION = 50; // jpg compression level
 
@@ -18,18 +18,19 @@ class Srcset
 	const RESIZE_CROP = "crop"; // resize by cropping, centered
 	const RESIZE_BOTH = "both"; // do 50% cropping centered, 50% resizing
 	const MAX_SCREEN_SIZE = 1920; // use to set a base calculation for $size = xxvw
+	const BREAK_SIZE = [992, 768, 576]; // break size of desktop, tablet, mobile
 	const SIZE = [100, 250, 500, 750, 1200, "src"];
 
 	// Must declare static because the main function is static => $this doen't exist
 	private static $savingPath;
 	private static $filename; // file name (without extension)
-	private static $sizeUsage; // 300
-	private static $sizeValue; // 300
-	private static $sizeType; // "vw", "px"
+	private static $sizeUsage = []; // 300 -> desktop, tablet, mobile
+	private static $sizeValue = []; // 300 -> desktop, tablet, mobile
+	private static $sizeType = []; // "vw", "px" -> desktop, tablet, mobile
 	private static $maxUsefullSize; // Minimum of size usage or img size => max usefull size
 
 
-	public static function src(string $filepath, string $size, $resizeParam = self::RESIZE_NORMAL, $resizeUpdate = null)
+	public static function src(string $filepath, array $size, $resizeParam = self::RESIZE_NORMAL, $resizeUpdate = null)
 	{
 		// Image construction
 		$img = new Image($_SERVER['DOCUMENT_ROOT'] . $filepath);
@@ -40,73 +41,90 @@ class Srcset
 		self::setSizeUsage($img, $size);
 		self::definePath();
 		self::resize($img, $resizeParam);
-		return self::generateHTML();
+		return self::generateHTML($img);
 	}
 
 
-	private static function setSizeUsage(Image $img, string $size)
+	private static function setSizeUsage(Image $img, array $size)
 	{
-		switch (substr($size, -2)) {
-			case 'px':
-				// Get the px value
-				self::$sizeValue = substr($size, 0, strlen($size) - 2);
-				self::$sizeUsage = self::$sizeValue;
-				self::$sizeType = "px";
-				break;
-			case 'vw':
-				self::$sizeValue = substr($size, 0, strlen($size) - 2);
-				self::$sizeUsage = self::MAX_SCREEN_SIZE * self::$sizeValue / 100;
-				self::$sizeType = "vw";
+		if (count($size) == 1 or count($size) == count(self::BREAK_SIZE)) {
+			for ($i = 0; $i < count($size); $i++) {
+				switch (substr($size[$i], -2)) {
+					case 'px':
+						// Get the px value
+						self::$sizeValue[$i] = substr($size[$i], 0, strlen($size[$i]) - 2);
+						self::$sizeUsage[$i] = self::$sizeValue[$i];
+						self::$sizeType[$i] = "px";
+						break;
+					case 'vw':
+						self::$sizeValue[$i] = substr($size[$i], 0, strlen($size[$i]) - 2);
+						self::$sizeUsage[$i] = self::MAX_SCREEN_SIZE * self::$sizeValue[$i] / 100;
+						self::$sizeType[$i] = "vw";
 
-				if (self::$sizeValue > 100) {
-					throw new \Exception("Viewport size can't exceed 100, arg passed: \"$size\"", 1);
+						if (self::$sizeValue[$i] > 100) {
+							throw new \Exception("Viewport size can't exceed 100, arg passed: \"$size[$i]\"", 1);
+						}
+						break;
+					default:
+						// If nothing if found, throw error
+						throw new \Exception("Wrong size, arg passed: \"$size[$i]\"", 1);
+						break;
 				}
-				break;
-			default:
-				// If nothing if found, throw error
-				throw new \Exception("Wrong size, arg passed: \"$size\"", 1);
-				break;
-		}
+			}
 
-		// Minimum of size usage or img size => max usefull size
-		self::$maxUsefullSize = min(self::$sizeUsage, $img->getWidth());
+			// If one size passed, affect every sizes to it
+			if (count($size) == 1) {
+				for ($i = 1; $i < count(self::BREAK_SIZE); $i++) {
+					self::$sizeValue[$i] = self::$sizeValue[$i - 1];
+					self::$sizeUsage[$i] = self::$sizeUsage[$i - 1];
+					self::$sizeType[$i] = self::$sizeType[$i - 1];
+				}
+			}
+
+			// Minimum of size usage or img size => max usefull size
+			self::$maxUsefullSize = min(max(self::$sizeUsage), $img->getWidth());
+		} else {
+			throw new \Exception("Wrong size param, expecting 1 or " . count(self::BREAK_SIZE) . " sizes, " . count($size) . " given", 1);
+		}
 	}
 
 	private static function definePath()
 	{
-		// $_SERVER['DOCUMENT_ROOT'] 	-> C:/wamp64/www/Gnicolas/
-		// $_SERVER['SCRIPT_FILENAME']	-> C:/wamp64/www/Gnicolas/index.php
-		$path = str_replace($_SERVER['DOCUMENT_ROOT'], "", $_SERVER['SCRIPT_FILENAME']);
-		$path = str_replace(".php", "", $path);
-		$path = self::DIR_SAVE_IMG . "/$path";
-		$path = explode("/", $path);
+		if (!isset(self::$savingPath)) {
+			// $_SERVER['DOCUMENT_ROOT'] 	-> C:/wamp64/www/Gnicolas/
+			// $_SERVER['SCRIPT_FILENAME']	-> C:/wamp64/www/Gnicolas/index.php
+			$path = str_replace($_SERVER['DOCUMENT_ROOT'], "", $_SERVER['SCRIPT_FILENAME']);
+			$path = str_replace(".php", "", $path);
+			$path = self::DIR_SAVE_IMG . "/$path";
+			$path = explode("/", $path);
 
-		// Folder path generation
-		$i = 0;
-		$savingPath = "";
-		while ($i < count($path)) {
-			if ($path[$i] == "") {
-				// Depile 1st element
-				array_shift($path);
-				continue;
+			// Folder path generation
+			$i = 0;
+			$savingPath = "";
+			while ($i < count($path)) {
+				if ($path[$i] == "") {
+					// Depile 1st element
+					array_shift($path);
+					continue;
+				}
+
+				$savingPath .= "/" . $path[$i];
+				if (!file_exists($_SERVER['DOCUMENT_ROOT'] . $savingPath)) {
+					mkdir($_SERVER['DOCUMENT_ROOT'] . $savingPath);
+				}
+
+				$i++;
 			}
 
-			$savingPath .= "/" . $path[$i];
-			if (!file_exists($_SERVER['DOCUMENT_ROOT'] . $savingPath)) {
-				mkdir($_SERVER['DOCUMENT_ROOT'] . $savingPath);
-			}
-
-			$i++;
+			self::$savingPath = $savingPath;
 		}
-
-		self::$savingPath = $savingPath;
 	}
 
 	private static function resize(Image $img, $resizeParam)
 	{
 		for ($i = 0; $i < count(self::SIZE); $i++) {
 			$SIZE = self::SIZE[$i];
-			$SIZE_PREC = self::SIZE[$i - 1] ?? 0;
+			// $SIZE_PREC = self::SIZE[$i - 1] ?? 0;
 
 			// Is resize update requested ?
 			$resizeUpdate = $resizeUpdate ?? self::RESIZE_UPDATE;
@@ -115,7 +133,7 @@ class Srcset
 			$resizeNeed = !file_exists(self::$savingPath . "/$SIZE" . "/" . self::$filename . ".jpg");
 
 			if ($resizeUpdate or $resizeNeed) {
-				if (!($SIZE == "src" or $SIZE_PREC < self::$maxUsefullSize or $SIZE < self::$maxUsefullSize)) {
+				if (!($SIZE == "src" or $SIZE < self::$maxUsefullSize)) {
 					continue;
 				} else {
 					// Check that the dst folder exists
@@ -177,13 +195,18 @@ class Srcset
 		}
 	}
 
-	private static function generateHTML()
+	private static function generateHTML(Image $img)
 	{
 		//------------------------------------------------------------------
 		// Print HTML code
 		//------------------------------------------------------------------
 		// sizes='100vw'
-		$return = "sizes='" . self::$sizeValue . self::$sizeType . "' ";
+		// sizes='(min-width:992px) 800px, (min-width:768px) 600px, (min-width:576px) 400px'
+		$return = "sizes='";
+		for ($i = 0; $i < count(self::BREAK_SIZE); $i++) {
+			$return .= "(min-width:" . self::BREAK_SIZE[$i] . "px) " . self::$sizeValue[$i] . self::$sizeType[$i] . ", ";
+		}
+		$return .= "' ";
 
 		// srcset="wolf-400.jpg 400w,"
 		$return .= "srcset='";
@@ -197,6 +220,7 @@ class Srcset
 					$return .= $path . " " . $SIZE . "w, ";
 				}
 			} else {
+				$return .= $path . " " . $img->getWidth() . "w, ";
 				$return .= "' ";
 
 				// src="wolf-400.jpg"
